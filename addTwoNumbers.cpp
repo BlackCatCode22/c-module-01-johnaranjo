1092f917435337283cbeaf339a99071d2a3ee66e
johnaranjo <john1397manuel@gmail.com>
2025-08-13 12:27:14 -0700

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,18 @@
+// Created by John Aranjo on 8/13/2025.
+// addTwoNumbers.cpp
+#include <iostream>
+using namespace std
+
+// create a very important function named main
+// this is where your program starts
+
+int main()
+
+int sumOfNums = 15;
+int num1 = 5;
+int num2 = 10;
+
+cout << "Enter First Number: ";
+num1 = cin.get()
+cout << "Enter Second Number: ";
+
