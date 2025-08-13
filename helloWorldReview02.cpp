598361b13cfe8e6f35c889395e99923259e9de7f
johnaranjo <john1397manuel@gmail.com>
2025-08-13 11:50:49 -0700

    Add files via upload

---
+++ helloWorldReview02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello world";
+
+    int a = 5;
+    int b = 10;
+    int c = 15;
+
+    cout << "a = " << a;
+    cout << "b = " << b;
+
+    int sum = a + b;
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
