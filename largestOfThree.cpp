99fa0559f16b4e2ed9b01e48cbd466ef6fd61337
johnaranjo <john1397manuel@gmail.com>
2025-08-18 12:00:54 -0700

    Add files via upload

---
+++ largestOfThree.cpp
@@ -0,0 +1,26 @@
+//Add 3 ints
+//JA 08/18
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "Hello and welcome to my largest of three ints program!" << endl;
+    //get three ints from the user
+    //create all variables needed
+    int num1;
+    int num2;
+    int num3;
+    int sumOfTheInts;
+
+    //initialize all variables
+    //assign a zero value to all variables
+    num1 = 0;
+    num2 = 0;
+    num3 = 0;
+    sumOfTheInts = 0;
+
+    cout << "\n please enter a value for num1: ";
+    cin >> num1;
+    cout << "\n you entered " << num1 << " for num1";
+    return 0;
+}
